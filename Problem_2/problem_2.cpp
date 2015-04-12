aa1cf7c271471befd740ffbd4d3bae1a39cd2e36
fugitiveinkc <blacksg1909@gmail.com>
2015-04-12 01:54:09 -0500

    Commented problem_2.cpp and problem_2.hs

--- Problem_2/problem_2.cpp
+++ Problem_2/problem_2.cpp
@@ -1,3 +1,10 @@
+/*
+
+Problem title: Even Fibonacci Numbers
+Summary: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -9,7 +16,7 @@ int main()
 	int fib_prev = 1;
 	while (fib_elem < 4000000)
 	{
-		
+
 		if (fib_elem%2 == 0)
 		{
 			fib_sum += fib_elem;
--- Problem_2/problem_2.hs
+++ Problem_2/problem_2.hs
@@ -1,5 +1,7 @@
+-- Problem title: Even Fibonacci Numbers
+-- Summary: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
 Steps:
 1) Fib sequence generator
 2) Remove all odd numbers
 3) Sum
- 
