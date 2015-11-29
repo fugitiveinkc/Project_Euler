72747861cdb9a1fcb52dd017b86ff7aa0151691b
fugitiveinkc <blacksg1909@gmail.com>
2015-11-28 21:38:28 -0600

    Redoing problem

--- Problem_81/path_sum_2.py
+++ Problem_81/path_sum_2.py
@@ -6,7 +6,8 @@ Title: Problem 81 -- Path sum: two ways
 Purpose: Only moving right and down and starting from the left top corner of a matrix, find the minimal path sum.
 
 Note:
-	-Problem with function
+	-Problem with function (misread problem!)
+	-Need to addred what happens when you get to second to last column.
 
 '''
 
@@ -15,47 +16,12 @@ Note:
 
 #Path sum traversal function (Recursive)
 
-def min_path_sum(row_index = 0, col_index = 0):
-	
-	try:
-		rightward = matrix[row_index][col_index + 1]
-
-	except IndexError:
-		rightward = None
-
-	try: 
-		downward = matrix[row_index + 1][col_index]
-
-	except IndexError:
-		downward = None
-
-	try:
-		current_position = matrix[row_index][col_index]
-	
-	except IndexError:
-		current_position = 0	
-
-	if rightward == None and downward == None:
-		return current_position
-
-	elif rightward > downward:
-		return current_position + min_path_sum(row_index + 1, col_index)
-
-	elif downward > rightward:
-		return current_position + min_path_sum(row_index, col_index + 1)
-
-	else:
-		return max([current_position + min_path_sum(row_index + 1, col_index), current_position + min_path_sum(row_index, col_index + 1)])
-	
-	
 
 
 #Read in matrix file
 
-matrix = open('p081_matrix.txt','r')
+matrix = open('test_matrix.txt','r')
 matrix = [row.strip('\n').split(',') for row in matrix]
 for row_index, row in enumerate(matrix):
 	for col_index, col in enumerate(row):
 		matrix[row_index][col_index] = int(col)
-solution = min_path_sum(0,0)		
-print solution
