dcdf7359473c0fd24a1e50ebee857a44ddd5b545
fugitiveinkc <blacksg1909@gmail.com>
2016-03-18 14:31:59 -0500

    Started

---
+++ Problem_17/solution1.py
@@ -0,0 +1,9 @@
+#Make useful list for int -> str conversion
+
+def string_count(number):
+	#Do stuff	
+	
+	
+
+if __name__ = "__main__":
+	print "Total number of letters used to spell out 1 through 1000: " + str(sum(string_count(x) for x in range(1,1001)))
