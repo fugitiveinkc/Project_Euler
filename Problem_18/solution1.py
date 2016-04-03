238af19ba5736fd9bd812252e09b0f408a287507
fugitiveinkc <blacksg1909@gmail.com>
2016-04-03 01:47:22 -0500

    Wrote using reverse of dijkstra's algorithm.  Still not working.

--- Problem_18/solution1.py
+++ Problem_18/solution1.py
@@ -5,7 +5,7 @@ Title: Problem 18 Maximum Path Sum
 Problem: Given a pyramid of numbers, find the maximumpath sum from top to bottom.
 
 Current State: Gives wrong answer (algorithm logic is wrong)
-	-Not using brute force.  
+	-Not using brute force.  (Using dijkstra's algorithm reverse)
 
 '''
 
@@ -22,13 +22,16 @@ def graph_generator(filename):
 		graph.append([])
 		for element in row:
 			if element[0] == '0':
-				graph[r].append(node(int(element[1:])))
+				graph[r].append(node(-int(element[1:])))
 			else:
-				graph[r].append(node(int(element)))
+				graph[r].append(node(-int(element)))
 	
 	for r in range(0, len(graph)-1): #Updates neighbors
 		for e in range(0, len(graph[r])):
 			graph[r][e].neighbors = [graph[r+1][e], graph[r+1][e+1]]
+
+	graph[0][0].distance = graph[0][0].value
+
 	return graph
 
 
@@ -39,24 +42,20 @@ def traverse_max(first_node):
 	while True:
 		current_sum = 0	
 		for a_node in visited: #Main traversal algorithm
-			#print a_node.value
+			print a_node.value
 			for b_node in a_node.neighbors:
-				if a_node.value + b_node.value > current_sum: #Problem here?
-					current_sum = a_node.value + b_node.value
-					current_node = a_node
-					next_node = b_node
+				if a_node.distance + b_node.value < b_node.distance:
+					b_node.distance = a_node.distance + b_node.value
+					if b_node.distance < current_sum:
+						current_sum = b_node.distance
+						current_node = a_node
+						next_node = b_node			
 		current_node.neighbors.remove(next_node)
 		next_node.previous = current_node
 		visited.append(next_node)
 		if not next_node.neighbors:
 			break
-	values = [visited[len(visited)-1].value]
-	previous = visited[len(visited)-1].previous
-	while previous != None: #Traverses maximum path and creates list of values
-		#print previous.value
-		values.append(previous.value)
-		previous = previous.previous	
-	return sum(values)
+	return next_node.distance*-1
 
 graph = graph_generator('pyramid.txt')
 print 'Maximum path sum: ' + str(traverse_max(graph[0][0]))
