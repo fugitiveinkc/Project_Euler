2fd5ab3ae9a07c540988b98f0dcdf92268fd745b
fugitiveinkc <blacksg1909@gmail.com>
2016-04-03 10:08:03 -0500

    Solution works. Used pyramid reduction algorithm.

---
+++ Problem_18/solution2.py
@@ -0,0 +1,31 @@
+'''
+
+Title: Solution 2 to Problem 18 -- Maximum Path Sum I
+
+Revision: Used pyramid simplification as algorithm
+	-How it works: Starts at the base and simplifies the next row by adding only feasible numbers from the row below it.  The pyramid becomes reduced by one row each iteration.
+
+'''
+
+#Read in pyramid
+
+pyramid = open('pyramid.txt')
+pyramid = [line.strip('\n').split(' ') for line in pyramid]
+pyramid = pyramid[::-1] #upside-down pyramid
+pyramid_int = []
+for r, row in enumerate(pyramid):
+	pyramid_int.append([])
+	for x in row:
+		pyramid_int[r].append(int(x))
+
+
+#Go through pyramid and simplify (start from bottom row and add top row based on only feasible solutions) -- pyramid has been inverted to make this easier
+
+for r, row in enumerate(pyramid_int[:len(pyramid_int)-1]):
+	for i in range(0,len(row)-1):
+		if row[i] > row[i+1]:
+			pyramid_int[r+1][i] += row[i]
+		else:
+			pyramid_int[r+1][i] += row[i+1]
+
+print 'Maximum path sum: ' + str(pyramid_int[::-1][0][0]) #Re-invert pyramid and top element should be solution
